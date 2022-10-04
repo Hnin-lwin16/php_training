e06b958d740e71e6087e99acfe2cb23c071b87e7
HninLwin <hninmohmohlwin16gmail.com>
2022-10-04 08:48:31 +0630

    added file tutorial_02

--- tutorials/tutorial_02.php
+++ tutorials/tutorial_02.php
@@ -1,9 +1,22 @@
-<style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loop</title>
+    <style>
     p{
         border:1px solid black;
         width : 100px;
+        margin: 0 auto;
     }
 </style>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
     echo "<p>";
     $star_size = 6;
